1b93bfefe2bfe9fa8098c42171769c72ee64808d
parisklein <92563216+parisklein@users.noreply.github.com>
2022-02-12 00:34:33 -0800

    Update DataInput.js

--- DataInput.js
+++ DataInput.js
@@ -8,148 +8,138 @@ import AddIcon from '@material-ui/icons/Add';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-    button: {
-        margin: theme.spacing(.1),
-    }
-}))
 
 
-function App(props) {
-    const classes = useStyles()
-    const inputArray = [{ Stakeholder: '', x: '', y: '' }, { Stakeholder: '', x: '', y: '' }]
-    const [inputFields, setInputFields] = useState([inputArray
-    ]);
-
-    function consoleItem(item, index, arr) {
-        console.log('a[' + index + ']=' + item);
-    }
-
-    const handleCalculate = (e) => {
-        e.preventDefault();
+    const boxStyle = {
+        border: '1px solid red',
+        padding: '5px'
+};
 
 
-        const calculateAverage = arr => arr.reduce((p, c) => p + c, 0) / arr.length;
+
+
+    const handleCalculate = (inputFields) => {
+
         let xValues = inputFields.map(function (obj) { return parseFloat(obj.x) })
         let yValues = inputFields.map(function (obj) { return parseFloat(obj.y) })
         var avg = { x: 0, y: 0 };
         avg.x = parseFloat(calculateAverage(xValues).toFixed(2));
         avg.y = parseFloat(calculateAverage(yValues).toFixed(2));
 
-        console.log("average of x:", avg.x);
-        console.log("average of y: ", avg.y);
-        //setResult(avg.x);
+        //console.log("average of x:", avg.x);
+        //console.log("average of y: ", avg.y);
 
-
-        const calculateRadiusSizes = function (avg, inputFields) {
-
-            let distances = inputFields.map(function (obj) {
-                return parseFloat(Math.sqrt((Math.pow((avg.x - obj.x), 2)) + (Math.pow((avg.y - obj.y), 2))).toFixed(2));
-            })
-
-            return distances;
-        }
-
-
+        // radisSizes is an array
         var radiusSizes = calculateRadiusSizes(avg, inputFields);
-        console.log("Radius: ", radiusSizes);
+        //console.log("Radius: ", radiusSizes);
 
-
+        return [radiusSizes, avg, xValues, yValues];
     };
 
+    const calculateRadiusSizes = function (avg, inputFields) {
 
-    const handleChangeInput = (index, event) => {
-        const values = [...inputFields];
-        values[index][event.target.name] = event.target.value;
-        setInputFields(values);
+        let distances = inputFields.map(function (obj) {
+            return parseFloat(Math.sqrt((Math.pow((avg.x - obj.x), 2)) + (Math.pow((avg.y - obj.y), 2))).toFixed(2));
+
+        })
+        //console.log(distances);
+        return distances; // returns distances in an array
 
     }
 
-    const handleAddFields = () => {
-        setInputFields([...inputFields, { Stakeholder: '', x: '', y: '' }])
+    const calculateAverage = arr => arr.reduce((p, c) => p + c, 0) / arr.length;
+
+//const handleAddFields = () => {
+//    setInputFields([...DataInput2.inputFields, { Stakeholder: '', x: '', y: '' }])
+//}
+
+
+export class DataInput2 extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputFields: [{ Stakeholder: '', x: '', y: '' }]
+        };
     }
 
-    const handleRemoveFields = (index) => {
-        const values = [...inputFields];
-        values.splice(index, 1);
-        setInputFields(values);
-    }
-    class App extends React.Component {
+    inputArray = { Stakeholder: '', x: '', y: '' , idx: ''}
 
-        state = {
-            value: ''
-        }
+    //inputArray = [{ Stakeholder: '', x: '', y: '' }, { Stakeholder: '', x: '', y: '' }]
+    //[inputFields, setInputFields] = useState([inputArray]);
+        //handleSubmit = () => {
+        //    const value = this.state.value;
+        //}
 
-        getValue = (event) => {
-            event.preventDefault();
-            console.log('Event: ', event.target.value);
-            this.setState({ value: event.target.value });
-
-        }
-
-        handleSubmit = () => {
-            const value = this.state.value;
-        }
+        handleClick() {
+            console.log("HEY THERE");
     }
 
+    handleAdd() {
+        this.state.inputFields.push({ Stakeholder: '', x: '', y: '' })
+        this.setState({ inputFields: this.state.inputFields })
+    }
 
+    handleRemove(index) {
+        
+        this.state.inputFields.splice(0, 1);
+        this.setState({ inputFields: this.state.inputFields });
+        //const values = this.setState({ inputFields: [this.state.inputFields] });
+    }
+        //handleClick(event) {
+        //    event.preventDefault();
+        //    var vars = handleCalculate();
+        //    this.props.updateSidebarData(vars[0], vars[1], vars[2], vars[3]);
+        //}
 
+    //inputFields = [{ Stakeholder: '', x: '', y: '' }]
 
-    const dataInput = (props) => {
+    render() {
         return (
-            <Container maxwidth="xs" className="input">
-                <form onCalculate={handleCalculate}>
-                    {inputFields.map((inputField, index) => (
-                        <div key={index}>
-                            <TextField
-                                name="Stakeholder"
-                                label="Stakeholder"
-                                variant='filled'
-                                value={inputField.Stakeholder}
-                                onChange={event => handleChangeInput(index, event)}
-                            />
-                            <TextField
-                                name="x"
-                                label="x"
-                                variant='filled'
-                                value={inputField.x}
-                                onChange={event => handleChangeInput(index, event) && this.getValue}
-
-
-                            />
-                            <TextField
-                                name="y"
-                                label="y"
-                                variant='filled'
-                                value={inputField.y}
-                                onChange={event => handleChangeInput(index, event)}
-                            />
-                            <IconButton
-                                onClick={() => handleRemoveFields(index)}
-                            >
+            <Container>
+                {this.state.inputFields.map((index) => (
+                    <div key={index}>
+                        <TextField
+                            name={index.x}
+                            label={index.x}
+                            variant='filled'
+                            value={index.x}
+                            //onChange={event => handleChangeInput(index, event)}
+                        />
+                        <TextField
+                            name="x"
+                            label="x"
+                            variant='filled'
+                            //value={inputField.x}
+                            //onChange={event => handleChangeInput(index, event) && this.getValue}
+                        />
+                        <TextField
+                            name="y"
+                            label="y"
+                            variant='filled'
+                            //value={inputField.y}
+                            //onChange={event => handleChangeInput(index, event)}
+                         />
+                        <IconButton
+                            onClick={this.handleRemove.bind(this)}
+                        >
                                 <RemoveIcon />
-                            </IconButton>
-                            <IconButton
-                                onClick={() => handleAddFields()}
-                            >
-                                <AddIcon />
-                            </IconButton>
-                        </div>
-                    ))}
-                    <Button
-                        className={classes.button}
-                        variant="contained"
-                        color="grey"
-                        type="submit" onClick={props.handleCalculate}>{props.title}
-                        Calculate
-                    </Button>
-
-                </form>
-
-
-            </Container>
-        )
+                        </IconButton>
+                        <IconButton
+                            onClick={this.handleAdd.bind(this)}
+                        >
+                            <AddIcon />
+                        </IconButton>
+                    </div>
+                ))}
+            <Button
+                variant="contained"
+                color="grey"
+                type="submit"
+                onClick={this.handleClick.bind(this)}>
+                Calculate
+                </ Button>
+        </Container>
+    )
     }
 }
-
-export default dataInput;
