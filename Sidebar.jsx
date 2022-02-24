45bfab73e9322c55699ba879e0764f9f7c6324c5
parisklein <92563216+parisklein@users.noreply.github.com>
2022-02-23 22:22:47 -0800

    Update Sidebar.jsx

--- Sidebar.jsx
+++ Sidebar.jsx
@@ -1,25 +1,37 @@
 import "./sidebar.css"
-
-import DataInput from "./DataInput"
-
-export default function Sidebar(props) {
-    return (
-        <div className="sidebar">
-            <div className="sidebarWrapper">
-                <div className="sidebarMenu">
-                    <h3 className="sidebarTitle">  Stakeholders</h3>
-                    <ul className="sidebarList">
-                        <div className="sidebarListItem">
-                            <DataInput {...props}/>
-                        </div>
-                    </ul>
+import React from 'react';
+import { DataInput2 } from "./DataInput2"
 
 
+export class Sidebar extends React.Component {
+    updateSidebarData(rSize, avg, xVal, yVal) {
+        this.props.updateAppData(rSize, avg, xVal, yVal);
+    }
+
+
+
+
+    render() {
+
+        const data = "hello from sidebar"
+
+        return (
+            <div className="sidebar">
+                <div className="sidebarWrapper">
+                    <div className="sidebarMenu">
+                        <h3 className="sidebarTitle">  Stakeholders</h3>
+                        <ul className="sidebarList">
+                            <div className="sidebarListItem">
+                                <DataInput2
+                                    data={data}
+                                    updateSidebarData={this.updateSidebarData.bind(this)}
+                                />
+                            </div>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        
 
-        </div>
-        
-        )
+        );
+    }
 }
